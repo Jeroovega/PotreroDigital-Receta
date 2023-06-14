f90d42daabde65168c262ed4561164f819e4eadb
Jeroovega <102939528+Jeroovega@users.noreply.github.com>
2023-06-13 21:36:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function cambiarColor(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "#fff";
+    document.getElementById("navBar").style.backgroundColor = "#c1b7b7";
+    document.getElementById("fondo-1").style.backgroundColor = "rgb(58, 56, 56)";
+    document.getElementById("fondo-2").style.backgroundColor = "rgb(58, 56, 56)";
+    document.getElementById("fondo-3").style.backgroundColor = "rgb(58, 56, 56)";
+    document.getElementById("fondo-4").style.backgroundColor = "rgb(58, 56, 56)";
+    document.getElementById("fondo-5").style.backgroundColor = "rgb(58, 56, 56)";
+    document.getElementById("subtitulo-1").style.color = "#fff";
+    document.getElementById("subtitulo-2").style.color = "#fff";
+    document.getElementById("subtitulo-3").style.color = "#fff";
+    document.getElementById("subtitulo-4").style.color = "#fff";
+    document.getElementById("subtitulo-5").style.color = "#fff";
+    document.getElementById("subtitulo-6").style.color = "#fff";
+    document.getElementById("link-1").style.color = "#000";
+    document.getElementById("link-2").style.color = "#000";
+    document.getElementById("link-3").style.color = "#000";
+    document.getElementById("link-4").style.color = "#000";
+};
+
+function infoPersonal(){
+    alert("Dueño de la página: Jerónimo Vega. Gmail: jerovega2001@gmail.com")
+}
